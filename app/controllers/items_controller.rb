767bbbbb229dcffe39a10e1eca25aef101b19c3d
Margo Urey <margo@margonline.co.uk>
2015-05-03 17:09:40 +0100

    index view for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Item.all  
+    @items = Item.order(created_at: :desc).paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
