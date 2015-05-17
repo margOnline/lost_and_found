ce6beb36874653f04603e25abad96ce9b5216c16
Margo Urey <margo@margonline.co.uk>
2015-05-17 21:01:24 +0100

    update tag_cloud to pass tag name as id param

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,5 +2,6 @@ class TagsController < ApplicationController
 
   def show
     @items = Item.tagged_with(params[:id])
+    Rails.logger.info("\n\n ******** #{@items.count }******** \n\n")
   end
 end
