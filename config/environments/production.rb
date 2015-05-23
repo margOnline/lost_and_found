17fcc6b90fc2d61d7031bbe3abdbd106621c4fff
Margo Urey <margo@margonline.co.uk>
2015-05-23 18:14:34 +0100

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.app.domain = "lost-and-found.com"
+  config.app_domain = "mau-lost-and-found.herokuapp.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
