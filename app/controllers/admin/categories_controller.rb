392a0a4d3e7b2338b2d2e165c58555c6805c437d
Margo Urey <margo@margonline.co.uk>
2015-07-05 14:25:27 +0100

    remove unused code

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,16 +2,13 @@ class Admin::CategoriesController < AdminController
   layout "admin"
 
   before_action :all_categories, only: [:index, :create]
-  before_action :setup_category, only: [:show, :edit, :update]
 
   def index
     @category = Category.new
-    authorize @category
   end
 
   def create
     @category = Category.new(categories_params)
-    authorize @category
      
     if @category.save
       respond_to do |format|
@@ -24,22 +21,9 @@ class Admin::CategoriesController < AdminController
     end
   end
 
-  def destroy
-    if @category.destroy
-      redirect_to admin_categories_path
-    else
-      render :edit
-    end
-  end
-
   private
   def categories_params
     params.require(:category).permit(:name)
-  end
-
-  def setup_category
-    @category = Category.find(categories_params)
-    authorize @category
   end
 
   def all_categories
